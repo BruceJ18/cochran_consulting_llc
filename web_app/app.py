2aa5896aac2c3471121bd855b64f634a6782045f
Bruce Jacobo <bruceja18@protonmail.com>
2023-06-09 12:28:09 -0700

    Handle Hashtag payload, add helper functions, complete basic add or edit logic and use .env for security.

--- web_app/app.py
+++ web_app/app.py
@@ -1,4 +1,5 @@
-import json
+import os
+from dataclasses import asdict
 from flask import (
     Flask,
     render_template,
@@ -21,12 +22,12 @@ from web_app.models import (
     Real_Estate
     )
 
+
 load_dotenv()
 
 app = Flask(__name__)
-client = MongoClient(
-    "mongodb+srv://brucej18:illescio369@cluster0.5bjmfpv.mongodb.net/")
-app.db = client.cochran_consulting_data
+client = MongoClient(os.environ.get("MONGODB_URI"))
+app.db = client.get_default_database()
 
 
 
@@ -39,7 +40,7 @@ app.config['SECRET_KEY'] = 'any secret string'
 def index():
     return render_template("index.html")
 
-# about us routes
+# about us route
 
 
 @app.route("/our_team")
@@ -49,11 +50,11 @@ def our_team():
         employees=app.db.employees.find({}, {"_id": False}))
 
 
-@app.route("/mindset")
-def mindset():
-    return render_template("/about_us/mindset.html")
+@app.route("/our_company_mindset")
+def our_company_mindset():
+    return render_template("/about_us/our_company_mindset.html")
 
-# listings routes
+# listings route
 
 
 @app.route('/business_listings_1')
@@ -79,7 +80,7 @@ def login():
 def add_or_edit():
     if request.method == "POST":
         collection = request.form.get("javascript_data")
-        entities = app.db[collection].find({}, {"_id": False})
+        entities = app.db[collection].find({})
 
         return render_template(
             "/web_app_mods/add_or_edit.html",
@@ -98,35 +99,54 @@ def selection():
 
         # add_or_edit.js POST REQUEST PAYLOAD DATA
         mod_selection = request.form.get('add_or_edit')
-        entity_id = request.form.get('entities_to_edit') 
         selected_form_edit = request.form.get('select--edit')
         selected_form_add = request.form.get('select--add')
+        entity_id = request.form.get('entities_to_edit') 
+        # ---------------------------------------------------
 
-        if (selected_form_add == '#' 
-            and selected_form_edit == '#' 
-            or entity_id == '#'):
-            return redirect( url_for('add_or_edit') ) # NEEDS ERROR MESSAGE
 
+        if ((selected_form_add == '#' 
+            and selected_form_edit == '#') 
+            or ( entity_id == '#' and selected_form_edit != '#' )):
+            return redirect( url_for('add_or_edit') ) # NEEDS ERROR MESSAGE ----------
+        
+        
+        # MODIFICATION SELECTION IS "add"
         if mod_selection == 'add':
             return redirect(url_for(
-                'add', 
-                selected_form_add=selected_form_add 
+                'add',
+                selected_form_add=selected_form_add,
+                mod_selection=mod_selection
                 ))
-
+        
+        # MODIFICATION SELECTION IS "edit"
         elif mod_selection == 'edit':
             return redirect(url_for(
                 'edit', 
                 entity_id=entity_id,
-                selected_form_edit=selected_form_edit 
+                selected_form_edit=selected_form_edit,
+                mod_selection=mod_selection
                 ))
 
         else:
-            return redirect(url_for('add_or_edit'))
+            return redirect(url_for('add_or_edit')) # NEEDS ERROR MESSAGE --------
         
 
 
 
-def get_form(selected_form: str, entity=None):
+def get_form(
+        selected_form: str, 
+        model : 
+        Client | 
+        Employee | 
+        Business | 
+        Real_Estate
+        =None ) -> (
+    Client_Form | 
+    Employee_Form | 
+    Business_Form | 
+    Real_Estate_Form
+    ):
     
     get_form_dict = {
     'clients' : Client_Form,
@@ -135,72 +155,168 @@ def get_form(selected_form: str, entity=None):
     'real_estate' : Real_Estate_Form
     }
 
-    return get_form_dict[selected_form](obj=entity)
+
+    return get_form_dict[selected_form](obj=model)
 
 
-def get_model(selected_form: str, entity_data=None):
+def get_model(selected_form: str, db_model_data=None):
 
-    get_form_dict = {
-    'clients' : client,
+    get_model_dict = {
+    'clients' : Client,
     'employees' : Employee,
     'businesses' : Business,
     'real_estate' : Real_Estate
     }
-    if not entity_data:
-        return get_form_dict[selected_form]
+
+    # if there is no form data (add entity selection guard)
+    if not db_model_data:
+        return get_model_dict[selected_form]
+    # --------------------------------------
     
-    return get_form_dict[selected_form](**entity_data)
+    return get_model_dict[selected_form](**db_model_data)
+
+
+def db_add_or_edit(selected_form: str, mod_selection: str, model, prev_id : int):
+
+    if mod_selection == 'add':
+        app.db[selected_form].insert_one(
+            {'info' : asdict(model)})
+
+    if mod_selection == 'edit':
+        app.db[selected_form].update_one(
+            {'info.id' : prev_id}, 
+            {"$set" : { 'info' : asdict(model)}})
         
+
+
+
+def update_model(selected_form: str, mod_selection: str, form, prev_id: int):
+    
+    if selected_form == 'employees':
         
+        model = Employee(
+        form.id.data,
+        form.name.data,
+        form.cell_number.data,
+        form.email.data,
+        form.background.data,
+        form.positions.data,
+        form.linkedin.data
+        )
+        db_add_or_edit(selected_form, mod_selection, model, prev_id)
+
+    elif selected_form == 'clients':
+        
+        model = Client(
+        form.id.data,
+        form.name.data,
+        form.business_name.data,
+        form.email.data,
+        form.cell_number.data,
+        form.website_address.data,
+        form.annual_revenue.data,
+        form.questions_or_comments.data
+        )
+
+        db_add_or_edit(selected_form, mod_selection, model, prev_id)
+        
+    
+    elif selected_form == 'businesses':
+        
+        model = Business(
+        form.id.data,
+        form.name.data,
+        form.business_desc.data,
+        form.link.data,
+        form.sold.data
+        )
+        db_add_or_edit(selected_form, mod_selection, model, prev_id)
+    
+    
+    elif selected_form == 'real_estate':
+        
+        model = Real_Estate(
+        form.id.data,
+        form.name.data,
+        form.price.data,
+        form.location.data,
+        form.rooms.data,
+        form.baths.data,
+        form.sqft.data,
+        form.sold.data
+        )
+        db_add_or_edit(selected_form, mod_selection, model, prev_id)
+
+    
+        
+
+
 @app.route("/add_or_edit/selection/add", methods=["GET", "POST"])
 def add():
-    selected_form_add = request.args.get('selected_form_add')
-
+    mod_selection = request.args.get('mod_selection')
+    selected_form_add = request.args.get('selected_form_add') 
     form = get_form(selected_form_add)
 
-
+    if form.validate_on_submit():
+        update_model(selected_form_add, mod_selection, form, None)
+        return redirect(url_for('add_or_edit')) # SUCCESS MESSAGE NEEDED --------------------
+    
     return render_template(
                 '/web_app_mods/add.html',
-                selected_form_add=selected_form_add,
-                form=form
+                form=form,
+                selected_form_add = selected_form_add,
+                mod_selection=mod_selection
             )
-
+        
 
 
 @app.route("/add_or_edit/selection/edit", methods=["GET", "POST"])
 def edit():
 
-    # DATA FROM add_or_edit/selection ENDPOINT
-    selected_form_edit = request.args.get('selected_form_edit')
+    # DATA FROM /add_or_edit/selection ENDPOINT
+    mod_selection : str = request.args.get('mod_selection')
+    selected_form_edit : str = request.args.get('selected_form_edit')
     entity_id = int(request.args.get('entity_id')) 
     # --------------------------------------------
         
-
+    # CURRENT DATA FROM MONGODB
     entity_data =(
         app.db[selected_form_edit]
-        .find({})[entity_id - 1]['info']
+        .find({'info.id' : entity_id})[0]['info']
         )
     
-    entity = get_model(
+    
+    model = get_model(
         selected_form_edit, 
-        entity_data=entity_data
+        db_model_data=entity_data
         )
     
     form = get_form(
         selected_form_edit, 
-        entity=entity
+        model
         )
 
-
     if form.validate_on_submit():
-        pass
+        if form.delete.data == 'Delete':
+            app.db[selected_form_edit].delete_many({'info.id' : entity_id})
+             
+            
+
+        update_model(selected_form_edit, mod_selection, form, entity_id)
+        return redirect(url_for('add_or_edit')) # SuCCESS MESSAGE NEEDED ----------------
+
 
 
     return render_template(
             '/web_app_mods/edit.html',
+            form=form,
+            model=model,
+            entity_id=entity_id,
             selected_form_edit=selected_form_edit,
-            form=form
+            mod_selection=mod_selection
         )
+
+    
 
 
 
